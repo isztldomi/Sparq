586045f422ebd74f45361cf2cc572074070692b6
Isztl Dominik <isztl.domi@gmail.com>
2026-03-25 15:13:09 +0100

    Add project files.

---
+++ Sparq.DataAccess/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparq.DataAccess.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string name) : base($"{name} entity not found.") { }
+    }
+}
---
+++ Sparq.DataAccess/Exceptions/SaveFailedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparq.DataAccess.Exceptions
+{
+    public class SaveFailedException : Exception
+    {
+        public SaveFailedException()
+        {
+        }
+
+        public SaveFailedException(string? message) : base(message)
+        {
+        }
+
+        public SaveFailedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
