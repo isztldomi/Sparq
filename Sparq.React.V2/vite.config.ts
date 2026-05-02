68a29434e87fc7eb1cb302d919324f5f2d847173
isztldomi <isztl.domi@gmail.com>
2026-05-02 18:21:10 +0200

    V2

--- Sparq.React.V2/vite.config.ts
+++ Sparq.React.V2/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
-import plugin from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   server: {
-    port: 57371,
+    proxy: {
+      "/api": {
+        target: "https://localhost:7218",
+        // target: "http://localhost:5200",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
